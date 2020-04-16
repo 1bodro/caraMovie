919f6a40c902899e3aedf48a3fd54d62dd870225
bodro <bodro@syneforge.com>
2020-04-16 21:37:54 +0300

    Updates

---
+++ precache-manifest.ee92612c51e43022ef7dbaa1742b5b5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f220c1234130d701585b",
+    "url": "/caraMovie/static/js/runtime~main.f5bab5b0.js"
+  },
+  {
+    "revision": "08c34d63f6d37b5c6c11",
+    "url": "/caraMovie/static/js/main.59726dc0.chunk.js"
+  },
+  {
+    "revision": "15f4b535606c79ba7009",
+    "url": "/caraMovie/static/js/2.1509a719.chunk.js"
+  },
+  {
+    "revision": "08c34d63f6d37b5c6c11",
+    "url": "/caraMovie/static/css/main.49ef10b4.chunk.css"
+  },
+  {
+    "revision": "15f4b535606c79ba7009",
+    "url": "/caraMovie/static/css/2.c170c7ab.chunk.css"
+  },
+  {
+    "revision": "df1864b37638cb1e338af0c9876cf72c",
+    "url": "/caraMovie/index.html"
+  }
+];
